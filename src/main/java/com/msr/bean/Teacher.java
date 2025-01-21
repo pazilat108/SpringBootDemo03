7d25b44a254250aa1aaef58064ccd4a8f575c8d7
zx12345 <zx@126.com>
2025-01-21 14:08:11 +0900

    创建了teacher的实体

---
+++ src/main/java/com/msr/bean/Teacher.java
@@ -0,0 +1,27 @@
+package com.msr.bean;
+
+/**
+ * @author acer
+ * @create 2025-01-21 14:06
+ * @desc
+ */
+public class Teacher {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
