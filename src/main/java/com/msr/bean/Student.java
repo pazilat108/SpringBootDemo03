554306b94bc9fc1b78f92cdc8556bb6b988f2104
zx12345 <zx@126.com>
2025-01-21 14:35:17 +0900

    创建了student实体

---
+++ src/main/java/com/msr/bean/Student.java
@@ -0,0 +1,18 @@
+package com.msr.bean;
+
+/**
+ * @author acer
+ * @create 2025-01-21 14:34
+ * @desc
+ */
+public class Student {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
